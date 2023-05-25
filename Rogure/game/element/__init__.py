897ef66511367ec9f1812f6e21675857a3715945
PatBiz <103768365+PatBiz@users.noreply.github.com>
2023-05-25 18:51:08 +0200

    Initialisation du projet.

---
+++ Rogure/game/element/__init__.py
@@ -0,0 +1,35 @@
+from .elem import Element
+from .creature import Creature, Monster, Hero
+from .equipment import Equipment
+from .decor import FixedElement, Stairs, Wall
+from .capacite import heal, teleport
+from .tools import meet
+
+__all__ = (
+    "Element",
+
+    # ---- Creature ----
+
+    "Creature",
+    "Monster",
+    "Hero",
+
+    # ---- Equipment ----
+
+    "Equipment",
+
+    # ---- Decor ----
+
+    "FixedElement",
+    "Stairs",
+    "Wall", 
+
+    # ---- Capacité ----
+
+    "heal",
+    "teleport",
+
+    # ---- Tools ----
+
+    "meet"
+)
---
+++ Rogure/game/element/elem.py
@@ -0,0 +1,28 @@
+#******************************* Importations : ********************************
+
+# Built-in modules :
+from typing import Optional
+
+
+#--------------------------------------------------------------------------------------------
+#                                         ELEMENT
+#--------------------------------------------------------------------------------------------
+#******************************* Importations : ********************************
+
+# Built-in modules :
+from typing import Optional
+
+
+class Element : #Classe abstraite
+    def __init__ (self , name:str , abbrv:Optional[str]=None) :
+        self._name = name
+        self._abbrv = abbrv or name[0]
+    
+    def __repr__ (self) :
+        return self._abbrv
+        
+    def description(self):
+        raise NotImplementedError("Element is an abstract class.")
+
+    def meet (self, hero) :
+        raise NotImplementedError("Element is an abstract class.")
